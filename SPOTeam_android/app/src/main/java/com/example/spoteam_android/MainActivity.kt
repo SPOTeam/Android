40d1b069710eb3702f12e5d34fd28b8284a176ec
starshape77 <111961655+starshape7@users.noreply.github.com>
2024-07-23 13:42:57 +0900

    change files and change img src

--- SPOTeam_android/app/src/main/java/com/example/spoteam_android/MainActivity.kt
+++ SPOTeam_android/app/src/main/java/com/example/spoteam_android/MainActivity.kt
@@ -12,12 +12,13 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.spoteam_android.databinding.ActivityMainBinding
-//import com.example.spoteam_android.ui.category.CategoryFragment
-//import com.example.spoteam_android.ui.category.StudyFragment
-//import com.example.spoteam_android.ui.home.HomeFragment
-//import com.example.spoteam_android.ui.community.CommunityHomeFragment
-//import com.example.spoteam_android.ui.mypage.BookmarkFragment
-//import com.example.spoteam_android.ui.mypage.MyPageFragment
+
+import com.example.spoteam_android.ui.category.CategoryFragment
+import com.example.spoteam_android.ui.category.StudyFragment
+import com.example.spoteam_android.ui.home.HomeFragment
+import com.example.spoteam_android.ui.community.CommunityHomeFragment
+import com.example.spoteam_android.ui.mypage.BookmarkFragment
+import com.example.spoteam_android.ui.mypage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,61 +30,61 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        init()
-        navController = findNavController(R.id.nav_host_fragment)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        bottomNavigationView.setupWithNavController(navController)
+        init()
+//        navController = findNavController(R.id.nav_host_fragment)
+//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+//        bottomNavigationView.setupWithNavController(navController)
     }
 
-//    private fun init() {
-//        val commitAllowingStateLoss = supportFragmentManager.beginTransaction()
-//            .replace(R.id.main_frm, CommunityHomeFragment())
-//            .commitAllowingStateLoss()
-//
-//        binding.mainBnv.setOnItemSelectedListener{ item ->
-//            when (item.itemId) {
-//
-////              HomeFragment
-//                R.id.navigation_home -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, CommunityHomeFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
-//                }
-////              categoryFragment
-//                R.id.navigation_category -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, CategoryFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
-//                }
-////              StudyFragment
-//                R.id.navigation_study -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, StudyFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
-//                }
-//
-////              찜Fragment
-//                R.id.navigation_bookmark -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, BookmarkFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
-//                }
-//
-////              MypageFragment
-//                R.id.navigation_mypage -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, MyPageFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
-//                }
-//            }
-//            false
-//        }
-//    }
+    private fun init() {
+        val commitAllowingStateLoss = supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HouseFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBnv.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+
+//              HomeFragment
+                R.id.navigation_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HouseFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+//              categoryFragment
+                R.id.navigation_category -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, CategoryFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+//              StudyFragment
+                R.id.navigation_study -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, StudyFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+//              찜Fragment
+                R.id.navigation_bookmark -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, BookmarkFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+//              MypageFragment
+                R.id.navigation_mypage -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, MyPageFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
 
 //    private fun init() {
 //        supportFragmentManager.beginTransaction()
