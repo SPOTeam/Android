d3533c6fb5f1002942f2a44cb6aad48bcaed158f
veen0219 <veen0219@naver.com>
2024-07-23 11:37:38 +0900

    house_fragment add

--- SPOTeam_android/app/src/main/java/com/example/spoteam_android/MainActivity.kt
+++ SPOTeam_android/app/src/main/java/com/example/spoteam_android/MainActivity.kt
@@ -3,56 +3,64 @@ package com.example.spoteam_android
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.spoteam_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-//        init()
-    }
+        navController = findNavController(R.id.nav_host_fragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
 
-//    private fun init() {
-//        supportFragmentManager.beginTransaction()
-//            .replace(R.id.main_frm, HomeFragment())
-//            .commitAllowingStateLoss()
-//
-//        binding.mainBnv.setOnItemSelectedListener{ item ->
+
+//        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+//            var selectedFragment: Fragment? = null
 //            when (item.itemId) {
-//
-//                R.id.home_fragment -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, HomeFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
+//                R.id.navigation_home -> {
+//                    selectedFragment = HouseFragment()
 //                }
-//
-//                R.id.dashboard_fragment -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, DashboardFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
+//                R.id.navigation_category -> {
+//                    selectedFragment = CategoryFragment()
 //                }
-//                R.id.notifications_fragment -> {
-//                    supportFragmentManager.beginTransaction()
-//                        .replace(R.id.main_frm, NotificationsFragment())
-//                        .commitAllowingStateLoss()
-//                    return@setOnItemSelectedListener true
+//                R.id.navigation_study -> {
+//                    selectedFragment = StudyFragment()
+//                }
+//                R.id.navigation_bookmark -> {
+//                    selectedFragment = BookmarkFragment()
+//                }
+//                R.id.navigation_mypage -> {
+//                    selectedFragment = MyPageFragment()
 //                }
 //            }
-//            false
+//            if (selectedFragment != null) {
+//                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+//            }
+//            true
 //        }
-//    }
+//
+//        if (savedInstanceState == null) {
+//            bottomNavigationView.selectedItemId = R.id.navigation_home
+//        }
+
+
+    }
+
+
+
 }
