0a4e0bf9531ffb6e25625a7b9098268eabd6427d
veen0219 <veen0219@naver.com>
2024-08-08 21:11:26 +0900

    confilct 해결

--- SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/community/CommunityCategoryVPAdapter.kt
+++ SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/community/CommunityCategoryVPAdapter.kt
@@ -2,7 +2,7 @@ package com.example.spoteam_android.ui.community
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.spoteam_android.ui.community.communityContent.AllFragment
+//import com.example.spoteam_android.ui.community.communityContent.AllFragment
 import com.example.spoteam_android.ui.community.communityContent.CounselingFragment
 import com.example.spoteam_android.ui.community.communityContent.FreeTalkFragment
 import com.example.spoteam_android.ui.community.communityContent.JobSeekingTalkFragment
@@ -17,7 +17,7 @@ class CommunityCategoryVPAdapter(fragment : Fragment) : FragmentStateAdapter(fra
     override fun createFragment(position: Int): Fragment {
         return when(position){
             //All
-            0 -> AllFragment()
+//            0 -> AllFragment()
             //PassingReview
             1 -> PassingReviewFragment()
             //ShareInfo
