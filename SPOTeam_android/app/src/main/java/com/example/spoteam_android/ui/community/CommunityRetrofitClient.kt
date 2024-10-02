7e26e01fbe29251aeb19fbbfd6a615fee18c1f64
veen0219 <veen0219@naver.com>
2024-10-02 13:26:59 +0900

    지라 테스트

--- SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/community/CommunityRetrofitClient.kt
+++ SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/community/CommunityRetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object CommunityRetrofitClient {
     private const val BASE_URL = "https://www.teamspot.site/"
 
-    private const val token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MTAsInRva2VuVHlwZSI6ImFjY2VzcyIsImlhdCI6MTcyNDQzMDUwMywiZXhwIjoxNzI0NTE2OTAzfQ.KwgNXIqriboA9B6Z4lF2obTqJ2hy3T5pbyt00d0QTFA"
+    private const val token = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MTAsInRva2VuVHlwZSI6ImFjY2VzcyIsImlhdCI6MTcyNzYxMzMwMywiZXhwIjoxNzI3Njk5NzAzfQ.qAHACpC4Rtw_dh6snxyKvXwy7ILD7M3u3B8vvWmA8oA"
 
     private val client = OkHttpClient.Builder().addInterceptor { chain ->
         val original = chain.request()
