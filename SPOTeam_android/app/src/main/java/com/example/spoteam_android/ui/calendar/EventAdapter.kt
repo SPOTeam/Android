d4948a1788a4364b181efa37ac497910515d6276
veen0219 <veen0219@naver.com>
2024-11-22 16:17:10 +0900

    [Fix]투두리스트 일정 Fetch 수정

--- SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/calendar/EventAdapter.kt
+++ SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/calendar/EventAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.spoteam_android.ui.calendar
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -52,6 +53,7 @@ class EventAdapter(
             eventTitleTextView.text = event.title
 
             if (selectedDate.isNotEmpty()) {
+                println("현재 선택된 날짜: $selectedDate")
             }
 
             // 이벤트의 날짜 정보를 yyyy-MM-dd 형식으로 변환
@@ -65,35 +67,43 @@ class EventAdapter(
                     } else {
                         when {
                             selectedDate == eventStartDate -> {
+                                Log.d("EventViewHolder", "Event 시작 날짜: $eventStartDate, 선택 날짜: $selectedDate")
                                 "${event.startDateTime} 시작"
                             }
                             selectedDate == eventEndDate -> {
+                                Log.d("EventViewHolder", "Event 종료 날짜: $eventEndDate, 선택 날짜: $selectedDate")
                                 "${event.endDateTime} 종료"
                             }
                             selectedDate > eventStartDate && selectedDate < eventEndDate -> {
+                                Log.d("EventViewHolder", "선택 날짜가 이벤트 중간에 해당: $selectedDate")
                                 "하루종일"
                             }
                             else -> {
+                                Log.d("EventViewHolder", "선택 날짜가 이벤트와 관련 없음: $selectedDate")
                                 ""
                             }
                         }
                     }
                 }
                 "DAILY", "WEEKLY", "BIWEEKLY", "MONTHLY" -> {
+                    Log.d("EventViewHolder", "반복 이벤트: ${event.period}, ${event.startDateTime} ~ ${event.endDateTime}")
                     "${event.startDateTime} ~ ${event.endDateTime}"
                 }
                 else -> {
+                    Log.d("EventViewHolder", "기타 이벤트: ${event.period}, ${event.startDateTime} ~ ${event.endDateTime}")
                     "${event.startDateTime} ~ ${event.endDateTime}"
                 }
             }
 
             if (event.isAllDay) {
+                Log.d("EventViewHolder", "하루종일 이벤트: $event")
                 eventTimeTextView.text = "하루종일"
             } else {
                 eventTimeTextView.text = eventTimeText
             }
 
             // 최종 텍스트 로그 출력
+            Log.d("EventViewHolder", "Event 시간 텍스트: $eventTimeText, 전체 텍스트: ${eventTimeTextView.text}")
 
         // 체크 아이콘 visibility 설정
             icCheck.visibility = if (isTodoList) View.GONE else View.VISIBLE
