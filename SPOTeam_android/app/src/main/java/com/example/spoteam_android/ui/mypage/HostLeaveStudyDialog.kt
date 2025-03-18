3c7837646ae744584406a27b9ee47fcc601b3847
veen0219 <veen0219@naver.com>
2025-03-18 14:19:34 +0900

    [Refactor] 버튼 color selector 추가

--- SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/mypage/HostLeaveStudyDialog.kt
+++ SPOTeam_android/app/src/main/java/com/example/spoteam_android/ui/mypage/HostLeaveStudyDialog.kt
@@ -1,27 +1,39 @@
 package com.example.spoteam_android.ui.mypage
 
+import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.Window
-import android.widget.TextView
+import android.widget.Button
+import android.widget.ImageView
 import com.example.spoteam_android.R
-import com.example.spoteam_android.ReportStudyMemberFragment
 
-class HostLeaveStudyDialog(private val context: Context, private val studyID : Int) {
+class HostLeaveStudyDialog(private val context: Context, private val studyID: Int) {
 
-    private val dlg = android.app.Dialog(context)
+    private val dlg = Dialog(context)
 
     fun start() {
         // 타이틀바 제거
         dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        // 커스텀 다이얼로그 radius 적용
+        // 배경을 투명하게 설정하여 radius 적용
         dlg.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-
         // 다이얼로그 레이아웃 설정
         dlg.setContentView(R.layout.dialog_host_leave_study)
 
         // 다이얼로그 표시
         dlg.show()
+
+        // X 버튼 클릭 시 다이얼로그 닫기
+        val ivClose: ImageView = dlg.findViewById(R.id.iv_close)
+        ivClose.setOnClickListener {
+            dlg.dismiss() // 다이얼로그 닫기
+        }
+
+        // 취소 버튼 클릭 시 다이얼로그 닫기
+        val btnCancel: Button = dlg.findViewById(R.id.btn_cancel)
+        btnCancel.setOnClickListener {
+            dlg.dismiss() // 다이얼로그 닫기
+        }
     }
 }
